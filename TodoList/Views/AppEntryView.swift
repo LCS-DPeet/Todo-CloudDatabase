24989ea3ecf820b759356718a584e35c15c34a6e
Danika Peet <DPeet@lcs.on.ca>
2024-05-30 08:47:45 -0400

    Added an authentication flow so that users are signed-in anonymously and assigned a unique user id. This prevents one user from seeing another user's data.

---
+++ TodoList/Views/AppEntryView.swift
@@ -0,0 +1,8 @@
+//
+//  AppEntryView.swift
+//  TodoList
+//
+//  Created by Danika Peet on 2024-05-27.
+//
+
+import Foundation
---
+++ TodoList/Views/AuthView.swift
@@ -0,0 +1,8 @@
+//
+//  AuthView.swift
+//  TodoList
+//
+//  Created by Danika Peet on 2024-05-27.
+//
+
+import Foundation
