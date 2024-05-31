8c7fb30c3a4e805f276c8da0877069526aec6c38
Danika Peet <DPeet@lcs.on.ca>
2024-05-31 12:21:33 -0400

    Can now select and load a photo from the Photos library.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -75,7 +75,7 @@ struct LandingView: View {
             // show the sheet to add a new item
             .sheet(isPresented: $presentingNewItemSheet) {
                 NewItemView(showSheet: $presentingNewItemSheet)
-                    .presentationDetents([.medium, .fraction(0.15)])
+                    .presentationDetents([.medium, .fraction(0.25)])
             }
             
             // Add a tool bar to the top of the interface
